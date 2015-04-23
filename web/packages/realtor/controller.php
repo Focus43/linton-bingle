02c9812a3ce4fe516f9655cd9f105ff188efc91d
Stine Richvoldsen <superrunt@gmail.com>
2015-04-23 09:34:54 -0600

    bump

--- web/packages/realtor/controller.php
+++ web/packages/realtor/controller.php
@@ -39,7 +39,7 @@
 
         protected $pkgHandle 			= self::PACKAGE_HANDLE;
         protected $appVersionRequired 	= '5.7';
-        protected $pkgVersion 			= '0.23';
+        protected $pkgVersion 			= '0.24';
 
 
         /**
