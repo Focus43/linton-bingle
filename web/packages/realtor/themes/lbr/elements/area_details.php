b4b782a538eefcf52a06beba492c33e55ffa141b
Stine Richvoldsen <superrunt@gmail.com>
2015-04-30 14:06:06 -0600

    update to are pages - check for valid file

--- web/packages/realtor/themes/lbr/elements/area_details.php
+++ web/packages/realtor/themes/lbr/elements/area_details.php
@@ -1,8 +1,8 @@
-<?php //var_dump($areaSubs); ?>
+<?php //var_dump($mastheadImages);exit; ?>
 <div class="area-details">
     <section class="hero">
         <div class="masthead" data-transition-speed="0.5"<?php if(!$isEditMode && (count($mastheadImages) > 1)){echo ' data-loop-timing="12"';} ?>>
-            <?php if(!empty($mastheadImages)): foreach($mastheadImages AS $index => $fileObj): ?>
+            <?php if(!empty($mastheadImages)): foreach($mastheadImages AS $index => $fileObj): if ( $fileObj && $fileObj->getRelativePath() ): ?>
                 <div class="node" style="background-image:url('<?php echo $fileObj->getRelativePath(); ?>');">
                     <div class="inner">
                         <div class="node-content">
@@ -10,13 +10,10 @@
                                 <h2><?php $index++; $a = new Area("Masthead Header {$index}"); $a->display($c); ?></h2>
                                 <?php $a = new Area("Masthead {$index}"); $a->display($c); ?>
                             </div>
-                            <!--                                <div class="visible-xs" data-viz-m>-->
-                            <!--                                    --><?php //$a = new Area("Masthead Mobile {$index}"); $a->display($c); ?>
-                            <!--                                </div>-->
                         </div>
                     </div>
                 </div>
-            <?php endforeach; endif; ?>
+            <?php endif; endforeach; endif; ?>
 
             <?php if(count($mastheadImages) > 1): ?>
                 <?php if ( $isEditMode ) { ?>
@@ -57,14 +54,6 @@
                     </div>
                 </div>
             <?php endforeach; endif; ?>
-
-<!--            --><?php //if(count($mastheadImages) > 1): ?>
-<!--                <div class="markers">-->
-<!--                    --><?php //for($i = 0; $i < count($mastheadImages); $i++): ?>
-<!--                        <a class="--><?php //echo $i === 0 ? 'active' : ''; ?><!--"><i class="icn-circle"></i></a>-->
-<!--                    --><?php //endfor; ?>
-<!--                </div>-->
-<!--            --><?php //endif; ?>
         </div>
     </section>
 </div>
