ba9f5035b83d84109aad5d13d5cd2439cb9eae6b
Stine Richvoldsen <superrunt@gmail.com>
2015-04-30 17:45:04 -0600

    higher zoom level on map

--- web/packages/realtor/themes/lbr/elements/property_details.php
+++ web/packages/realtor/themes/lbr/elements/property_details.php
@@ -87,7 +87,7 @@
                 var propLatLng = new google.maps.LatLng(<?php echo $propertyObj->getLatitude(); ?>,<?php echo $propertyObj->getLongitude(); ?>);
                 var mapOptions = {
                     center: propLatLng,
-                    zoom: 9
+                    zoom: 12
                 };
                 var map = new google.maps.Map(document.getElementById('gmapCanvas'),
                     mapOptions);
