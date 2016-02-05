1d3dbaa0494833bae89ede13930ccf38902e792e
Sam Fitz <abigwonderful@gmail.com>
2016-02-04 17:29:44 -0700

    trying to get file manager up and running

--- web/application/config/app.php
+++ web/application/config/app.php
@@ -1,9 +1,9 @@
 <?php
 
 return array(
-    'providers' => array(
-        'core_session' => '\Application\Src\Session\SessionServiceProvider'
-    ),
+    // 'providers' => array(
+    //     'core_session' => '\Application\Src\Session\SessionServiceProvider'
+    // ),
     'theme_paths'         => array(
         '/page_not_found' => 'lbr'
     )
